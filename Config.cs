a772b2acf6430bb25f1ea3cfded0be7e81a86717
Sl4vP0weR <42337892+Sl4vP0weR@users.noreply.github.com>
2022-05-11 21:55:58 +0800

    Initial commit.

---
+++ Config.cs
@@ -0,0 +1,13 @@
+﻿namespace SBarricadeInfo;
+
+/// <summary>
+/// This class is XML serializing to config file. <br/>
+/// Docs: <see href="https://docs.microsoft.com/en-us/dotnet/api/system.xml.serialization.xmlserializer?view=netframework-4.7.2"/>
+/// </summary>
+public partial class Config : IRocketPluginConfiguration
+{
+    public string Permission = "barricade_info";
+    public void LoadDefaults()
+    {
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+
+namespace SBarricadeInfo;
+
+public sealed partial class Main : RocketPlugin<Config>
+{
+    protected override void Unload()
+    {
+        UnturnedPlayerEvents.OnPlayerUpdateGesture -= GestureUpdatedHandler;
+    }
+    protected override void Load()
+    {
+        UnturnedPlayerEvents.OnPlayerUpdateGesture += GestureUpdatedHandler;
+    }
+
+    static async void GestureUpdatedHandler(UnturnedPlayer player, UnturnedPlayerEvents.PlayerGesture gesture)
+    {
+        // only when player is Pointing
+        if (gesture != UnturnedPlayerEvents.PlayerGesture.Point)
+            return;
+        // checking for access
+        if (!player.HasPermission(conf.Permission))
+            return;
+        // searching for barricade
+        var aim = player.Player.look.aim;
+        var raycastInfo = DamageTool.raycast(new(aim.position, aim.forward), 5f, RayMasks.BARRICADE);
+        // validating barricade
+        var hitTransform = raycastInfo.transform;
+        if (!hitTransform)
+            return;
+        var barricadeDrop = BarricadeManager.FindBarricadeByRootTransform(hitTransform);
+        if (barricadeDrop is null)
+            return;
+        // getting all info that we need
+        var barricadeAsset = barricadeDrop.asset;
+        var barricadeData = barricadeDrop.GetServersideData();
+        var barricade = barricadeData.barricade;
+        var barricadeOwnerId = new CSteamID(barricadeData.owner);
+        // sending formatted info
+        UnturnedChat.Say(player, TranslateInfo(barricadeAsset.itemName, barricadeAsset.id, barricade.health, TryGetOwnerName(barricadeOwnerId), barricadeOwnerId));
+    }
+    public static string TryGetOwnerName(CSteamID id)
+    {
+        var target = PlayerTool.getPlayer(id);
+        return target ? target.channel.owner.playerID.characterName : TranslateUnknown();
+    }
+}
